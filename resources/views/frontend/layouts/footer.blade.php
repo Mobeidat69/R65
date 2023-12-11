6cbf316cc9a00e8e6562fdb0e550f3aa5e237a87
Mobeidat69 <moh.obeidat079@gmail.com>
2023-12-11 21:30:41 +0300

    first

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,163 @@
+
+	<!-- Start Footer Area -->
+	<footer class="footer">
+		<!-- Footer Top -->
+		<div class="footer-top section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 col-md-6 col-12">
+						<!-- Single Widget -->
+						<div class="single-footer about">
+							<div class="logo">
+								<a href="index.html"><img src="{{asset('backend/img/balqis.png')}}" alt="#"></a>
+							</div>
+							@php
+								$settings=DB::table('settings')->get();
+							@endphp
+							<p class="text">
+								Balqis Company for New Technology was established in 2008, a Jordanian based company, providing integrated high-quality security solutions and products in Jordan and the region, In addition to providing technical support at competitive prices.
+
+								Rapidly growing company that is specialized in providing professional surveillance solutions, premium sound system, Fire Alarm, intrusion Alarm Home Automation and all other security solutions
+							</p>
+							<p class="call">Got Question? Call us 24/7<span><a href="tel:+962798855444">+962798855444</a></span></p>
+						</div>
+						<!-- End Single Widget -->
+					</div>
+					<div class="col-lg-2 col-md-6 col-12">
+						<!-- Single Widget -->
+						<div class="single-footer links">
+							<h4>Information</h4>
+							<ul>
+								<li><a href="{{route('about-us')}}">About Us</a></li>
+								<li><a href="#">Faq</a></li>
+								<li><a href="#">Terms & Conditions</a></li>
+								<li><a href="{{route('contact')}}">Contact Us</a></li>
+								<li><a href="#">Help</a></li>
+							</ul>
+						</div>
+						<!-- End Single Widget -->
+					</div>
+					<div class="col-lg-2 col-md-6 col-12">
+						<!-- Single Widget -->
+						<div class="single-footer links">
+							<h4>Customer Service</h4>
+							<ul>
+								<li><p style="color: white">Payment Methods</p></li>
+								<li><p style="color: white">Money-back</p></li>
+								<li><p style="color: white">Returns</p></li>
+								<li><p style="color: white">Shipping</p></li>
+								<li><p style="color: white">Privacy Policy</p></li>
+							</ul>
+						</div>
+						<!-- End Single Widget -->
+					</div>
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Single Widget -->
+						<div class="single-footer social">
+							<h4>Get In Tuch</h4>
+							<!-- Single Widget -->
+							<div class="contact">
+								<ul>
+									<li>K. Hussein St., Aqaba , jordan</li>
+									<li>@foreach($settings as $data) {{$data->email}} @endforeach</li>
+									<li>+962798855444</li>
+								</ul>
+							</div>
+							<!-- End Single Widget -->
+							<p style="color: white">Follow Us</p>
+							<div class="sharethis-inline-follow-buttons">
+								<a href="#" style="color: white;font-size:2rem"><i class="fa-brands fa-facebook"></i></a>
+								<a href="#" style="color: white;font-size:2rem"><i class="fa-brands fa-whatsapp"></i></a>
+								<a href="#" style="color: white;font-size:2rem"><i class="fa-brands fa-instagram"></i></a>
+							</div>
+						</div>
+						<!-- End Single Widget -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Footer Top -->
+		<div class="copyright">
+			<div class="container">
+				<div class="inner">
+					<div class="row">
+						<div class="col-lg-6 col-12">
+							<div class="left">
+								<p>Copyright © {{date('Y')}} <span>BALQIS TECHNOLOGY</span>  -  All Rights Reserved.</p>
+							</div>
+						</div>
+						<div class="col-lg-6 col-12">
+							<div class="right">
+								<img src="{{asset('backend/img/payments.png')}}" alt="#">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- /End Footer Area -->
+ 
+	<!-- Jquery -->
+    <script src="{{asset('frontend/js/jquery.min.js')}}"></script>
+    <script src="{{asset('frontend/js/jquery-migrate-3.0.0.js')}}"></script>
+	<script src="{{asset('frontend/js/jquery-ui.min.js')}}"></script>
+	<!-- Popper JS -->
+	<script src="{{asset('frontend/js/popper.min.js')}}"></script>
+	<!-- Bootstrap JS -->
+	<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+	<!-- Color JS -->
+	<script src="{{asset('frontend/js/colors.js')}}"></script>
+	<!-- Slicknav JS -->
+	<script src="{{asset('frontend/js/slicknav.min.js')}}"></script>
+	<!-- Owl Carousel JS -->
+	<script src="{{asset('frontend/js/owl-carousel.js')}}"></script>
+	<!-- Magnific Popup JS -->
+	<script src="{{asset('frontend/js/magnific-popup.js')}}"></script>
+	<!-- Waypoints JS -->
+	<script src="{{asset('frontend/js/waypoints.min.js')}}"></script>
+	<!-- Countdown JS -->
+	<script src="{{asset('frontend/js/finalcountdown.min.js')}}"></script>
+	<!-- Nice Select JS -->
+	<script src="{{asset('frontend/js/nicesellect.js')}}"></script>
+	<!-- Flex Slider JS -->
+	<script src="{{asset('frontend/js/flex-slider.js')}}"></script>
+	<!-- ScrollUp JS -->
+	<script src="{{asset('frontend/js/scrollup.js')}}"></script>
+	<!-- Onepage Nav JS -->
+	<script src="{{asset('frontend/js/onepage-nav.min.js')}}"></script>
+	{{-- Isotope --}}
+	<script src="{{asset('frontend/js/isotope/isotope.pkgd.min.js')}}"></script>
+	<!-- Easing JS -->
+	<script src="{{asset('frontend/js/easing.js')}}"></script>
+
+	<!-- Active JS -->
+	<script src="{{asset('frontend/js/active.js')}}"></script>
+	<script src="https://kit.fontawesome.com/a342f74175.js" crossorigin="anonymous"></script>
+	
+	@stack('scripts')
+	<script>
+		setTimeout(function(){
+		  $('.alert').slideUp();
+		},5000);
+		$(function() {
+		// ------------------------------------------------------- //
+		// Multi Level dropdowns
+		// ------------------------------------------------------ //
+			$("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+				event.preventDefault();
+				event.stopPropagation();
+
+				$(this).siblings().toggleClass("show");
+
+
+				if (!$(this).next().hasClass('show')) {
+				$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+				}
+				$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+				$('.dropdown-submenu .show').removeClass("show");
+				});
+
+			});
+		});
+	  </script>
